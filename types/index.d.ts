0d05d45464ab03573579c3f7b5c48748a11ddfd6
Iago Belo <iagobelo.oliveira@gmail.com>
2021-08-13 09:32:02 -0300

    chore: delete build files

--- types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import lens from './lens';
-import set from './set';
-import over from './over';
-import view from './view';
-import compose from './compose';
-import prop from './prop';
-export { lens, set, over, view, compose, prop };
--- types/view.d.ts
+++
@@ -1,8 +0,0 @@
-import { Lens } from './lens';
-declare type LensView = <O, V>(lens: Pick<Lens<O, V>, 'get'>) => (data: O) => V;
-/**
- * Returns the property using the given lens.
- * @param lens - Lens to get (view) the property.
- */
-declare const view: LensView;
-export default view;
